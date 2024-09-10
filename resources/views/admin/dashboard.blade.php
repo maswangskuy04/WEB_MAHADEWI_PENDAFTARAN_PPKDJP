1be17b405426528c82650c81e680d6425342ab64
98workdriver98 <98workdriver98@gmail.com>
2024-09-10 17:03:08 +0200

    update cuyy

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,7 +10,7 @@
         <div class="card-body">
             <h5 class="card-title">Special title treatment</h5>
             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn btn-primary">Go somewherew</a>
         </div>
     </div>
 @endsection
